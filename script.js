eb83230f06df86322426fe47f6b3e47d6f9ae143
Shrinivas Kathare <97666522+shrinivask007@users.noreply.github.com>
2025-06-18 10:38:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Person {}
+class Person { 
+	constructor(name, age){
+		this.name=name;
+		this.age=age;
+	}
+	get(){
+		return this.name;
+	}
+	set(newage){
+		this.age=newage;
+	}
+}
+class Student extends Person {
+	study(){
+		consloe.log(`${this.name} is studing`)
+	}
+}
 
-class Student extends Person {}
-
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
